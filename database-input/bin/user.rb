b4ce21a9044acde33f5163708d4abb8d720d913c
Elizabeth Davis <davise10@tcnj.edu>
2016-09-30 16:11:35 -0400

    puts user input into database table

---
+++ database-input/bin/user.rb
@@ -0,0 +1,48 @@
+require 'sinatra'
+require 'sqlite3'
+
+set :port, 8080
+set :static, true
+set :public_folder, "static"
+set :views, "views"
+
+get '/' do
+    return 'Hello world'
+end
+
+get '/hello/' do 
+    erb :hello_form
+end
+
+post '/hello/' do
+    greeting = params[:greeting] || "Hi There"
+    name = params[:name] || "Nobody"
+    erb :index, :locals => {'greeting' => greeting, 'name' => name}
+end
+
+get '/hello-database/' do 
+    erb :hello_form
+end 
+
+post '/hello-database/' do
+    greeting = params[:greeting] || "Hi There"
+    name = params[:name] || "Nobody"
+    erb :index, :locals => {'greeting' => greeting, 'name' => name}
+
+    begin
+    	db = SQLite3::Database.open "users.db"
+    	db.execute "CREATE TABLE IF NOT EXISTS People(Id INTEGER PRIMARY KEY, 
+        Name TEXT, Greeting TEXT)"
+        db.execute "INSERT INTO People VALUES(1,#{name},#{greeting}, name, greeting)"
+    puts "Your data has been entered into the database!"
+
+    rescue SQLite3::Exception => e 
+    
+    puts "Exception occurred"
+    puts e
+    
+	ensure
+	    db.close if db
+	end
+end
+
