4654bcbf83d613296326c62445e17fe5fd95446a
Elizabeth Davis <davise10@tcnj.edu>
2016-09-30 15:25:56 -0400

    Accessing an SQLite database with Ruby

---
+++ ruby-sqlite-accessingDB/fetch.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+#Fetches the data from Cars, limiting to 5 rows
+
+require 'sqlite3'
+
+begin
+    
+    db = SQLite3::Database.open "test.db"
+    
+    stm = db.prepare "SELECT * FROM Cars LIMIT 5" 
+    rs = stm.execute 
+    
+    rs.each do |row|
+        puts row.join "\s"
+    end
+           
+rescue SQLite3::Exception => e 
+    
+    puts "Exception occurred"
+    puts e
+    
+ensure
+    stm.close if stm
+    db.close if db
+end
---
+++ ruby-sqlite-accessingDB/lastrow.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+#Creates a table and inserts values into it, printing out the last id of the last inserted row
+
+require 'sqlite3'
+
+begin
+    
+    db = SQLite3::Database.new ":memory:"
+    
+    db.execute "CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT)"
+    db.execute "INSERT INTO Friends(Name) VALUES ('Tom')"
+    db.execute "INSERT INTO Friends(Name) VALUES ('Rebecca')"
+    db.execute "INSERT INTO Friends(Name) VALUES ('Jim')"
+    db.execute "INSERT INTO Friends(Name) VALUES ('Robert')"
+    db.execute "INSERT INTO Friends(Name) VALUES ('Julian')"
+    
+    id = db.last_insert_row_id
+    puts "The last id of the inserted row is #{id}"
+    
+rescue SQLite3::Exception => e 
+    
+    puts "Exception occurred"
+    puts e
+    
+ensure
+    db.close if db
+end
